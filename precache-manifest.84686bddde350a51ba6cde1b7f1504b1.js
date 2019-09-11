28eec22e7e9da9f87d7224612a5aebcd32e7ba5c
Paulina <p.kijowska@gmail.com>
2019-09-11 10:48:59 +1000

    Updates

---
+++ precache-manifest.84686bddde350a51ba6cde1b7f1504b1.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3a97f020fe616cc550712223e6e9d8e",
+    "url": "/courageworks/index.html"
+  },
+  {
+    "revision": "cab777568e88a035e344",
+    "url": "/courageworks/static/css/2.70fc37fa.chunk.css"
+  },
+  {
+    "revision": "c3e432c8029d0f2e757a",
+    "url": "/courageworks/static/css/main.b12e1925.chunk.css"
+  },
+  {
+    "revision": "cab777568e88a035e344",
+    "url": "/courageworks/static/js/2.f3014b72.chunk.js"
+  },
+  {
+    "revision": "c3e432c8029d0f2e757a",
+    "url": "/courageworks/static/js/main.96aa2dd1.chunk.js"
+  },
+  {
+    "revision": "a9f95f4182d0efd63be0",
+    "url": "/courageworks/static/js/runtime~main.4847e111.js"
+  },
+  {
+    "revision": "4b29ac7501bd20978bf4393606ac27ee",
+    "url": "/courageworks/static/media/baloon_girl_banksy.4b29ac75.jpg"
+  },
+  {
+    "revision": "21cea2eb4723ce0db5c5908107914751",
+    "url": "/courageworks/static/media/exists_igor.21cea2eb.jpg"
+  },
+  {
+    "revision": "59adcb6bae222cf472fc7719113abc13",
+    "url": "/courageworks/static/media/flower_banksy.59adcb6b.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/courageworks/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/courageworks/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/courageworks/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/courageworks/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/courageworks/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "469ed2451c16ef8c187f53b352985287",
+    "url": "/courageworks/static/media/hush_1.469ed245.jpg"
+  },
+  {
+    "revision": "585e47d5c3746d868561dbb783f7116e",
+    "url": "/courageworks/static/media/hush_2.585e47d5.jpg"
+  },
+  {
+    "revision": "5e7380d2611be477b8b85be50879867e",
+    "url": "/courageworks/static/media/hush_3.5e7380d2.jpg"
+  },
+  {
+    "revision": "12851554750ce8c5bedf8159b1130f19",
+    "url": "/courageworks/static/media/migration_birds.12851554.jpg"
+  },
+  {
+    "revision": "579b57a4217f7cc5abd8389eeb2534f3",
+    "url": "/courageworks/static/media/only_love_igor.579b57a4.jpg"
+  },
+  {
+    "revision": "424889b085b12e4e29e6d63a16743cf4",
+    "url": "/courageworks/static/media/senses_igor.424889b0.jpg"
+  }
+]);
